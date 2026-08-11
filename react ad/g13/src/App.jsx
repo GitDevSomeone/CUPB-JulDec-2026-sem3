94a9ca6e687cc596093ed4494759e2b19bc95bec
himanshu <himanshu11199@gmail.com>
2026-08-11 15:17:01 +0530

    sepereated folder

---
+++ react ad/g13/src/App.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+let students = [{
+  name: "Himanshu",
+  group: "g13",
+  marks: 20
+},
+{
+  name: "Ritik",
+  group: "g13",
+  marks: 12
+},
+{
+  name: "Hemant",
+  group: "g13",
+  marks: 15
+}]
+
+function App(){
+  return(
+    <>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Group</th>
+            <th >Marks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* border: value.marks >= 15 ? "1px solid green" : "1px solid red", */}
+          {students.map(function(value, index){
+            return (
+              <tr style={{
+               backgroundColor: value.marks > 15 ?"green" : "red"
+              }}>
+                <td>{value.name}</td>
+                <td>{value.group}</td>
+                <td>{value.marks}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
+
+
+
+// function App(){
+//   return(
+    // <>
+    //  {students.map(function(value, index, array){
+    //    return (<div style={{
+    //     height:"200px",
+    //     width: "150px",
+    //     border: value.marks >= 15 ? "1px solid green" : "1px solid red",
+    //     textAlign: "center",
+    //     display: value.marks >= 15 ? "block": "none"
+    //   }}>
+    //     <p>name: {students[index].name}</p>
+    //     <p>group: {value.group}</p>
+    //     <p>marks: {value.marks}</p>
+    //   </div>)
+    //  })}
+    // </>
+//   )
+// }
+
+// export default App
