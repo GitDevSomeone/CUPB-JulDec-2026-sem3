82c76be87642f1c606b56af02c68c575ad6c567b
himanshu <himanshu11199@gmail.com>
2026-08-17 17:26:08 +0530

    g13

--- react ad/g13/src/components/Counter.jsx
+++ react ad/g13/src/components/Counter.jsx
@@ -3,10 +3,34 @@ import {useState} from "react"
 
 function Counter() {
 
-    let [count, setCount] = useState("himanshu")
+    let [count, setCount] = useState(0)
+    let [string, setString] = useState("")
+    let [bool, setBool] = useState(false)
 
     function clickHandler(){
-        setCount(count * 1)
+        // setCount(count + 1)
+        // setCount(count + 1) 
+        // setCount(count + 1)
+
+        setCount(function(prev){
+          return prev + 1
+        })
+       
+    }
+
+    function inputHandler(event){
+      console.log(event.target.value)
+      setString(function(){
+        return event.target.value
+      })
+    }
+
+    function boolHandler(){
+
+       setBool((prev) => !prev)
+      // setBool(function(prev){
+      //   return false
+      // })
     }
 
   return (
@@ -14,6 +38,13 @@ function Counter() {
       {/* <button onclick="clickHandler()">click</button>  HTML way */}
       <h1>{count}</h1>
       <button onClick={clickHandler}>+</button>
+
+      <input type='text' onChange={inputHandler} />
+
+      <p>{string}</p>
+
+      <p>{bool == true ? "true" : "false"}</p>
+      <button onClick={boolHandler}>change boolean value</button>
 
     </div>
   )
